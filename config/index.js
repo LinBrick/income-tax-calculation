882364e3fb7c045b502cf4ff83ae95d7e66a192e
linwenyao <linwenyao@flytexpress.com>
2019-01-12 17:36:15 +0800

    配置打包路径

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './income-tax-calculation-view/',
+    assetsPublicPath: '/income-tax-calculation-view/',
 
     /**
      * Source Maps
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Index from '@/components/Index'
 Vue.use(Router)
 
 export default new Router({
+  base: '/income-tax-calculation-view/',
   routes: [
     {
       path: '/',
